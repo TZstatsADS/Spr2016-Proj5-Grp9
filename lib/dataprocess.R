bf239c78ebddb0a2f8e85c14ce44849dfb3d0de6
andywan2016 <zw2327@columbia.edu>
2016-04-24 06:47:29 -0400

    final version

--- lib/dataprocess.R
+++ lib/dataprocess.R
@@ -1,25 +1,37 @@
 #this is an empty file
 
 library(dplyr)
-setwd("C:/ADS/project5/project5/files")
-tmp=read.csv(file="1.csv",head=TRUE,sep=",")
+
+filelist=dir("../data/",pattern=".csv")
+for(i in 1:length(filelist))
+{
+ tmp=read.csv(file=paste("../data/",filelist[i],sep=""),header = TRUE ,sep = ",")
+ tmp$date=as.character(tmp$date)
+ tmp$ip_id=as.character(tmp$ip_id)
+ tmp$package=as.character(tmp$package)
+ 
+ DateAndID=paste(tmp$date,tmp$ip_id)
+ 
+ newTable=cbind(DateAndID,tmp)
+ 
+ GroupedTable=newTable%>%
+   group_by(DateAndID)%>%
+   summarise(pack=paste(package, collapse = ' '))
+ 
+ write.table(GroupedTable,file="../data/dataoutput/DateUserPack.csv",sep=",",col.names = FALSE,row.names = FALSE,append = TRUE)
+}
 
 #change data type
-
 #tmp1=tmp%>%
 #  mutate_each_(funs(as.character),date)
 
 
-tmp$date=as.character(tmp$date)
-tmp$ip_id=as.character(tmp$ip_id)
-tmp$package=as.character(tmp$package)
+#DateAndID=paste(tmp$date,tmp$ip_id)
 
-DateAndID=paste(tmp$date,tmp$ip_id)
+#newTable=cbind(DateAndID,tmp)
 
-newTable=cbind(DateAndID,tmp)
+#head(newTable,n=3)
 
-head(newTable,n=3)
 
-GroupedTable=newTable%>%
-  group_by(DateAndID)%>%
-  summarise(pack=paste(package, collapse = ' '))
+
+#write.table(GroupedTable,file="../data/dataoutput/DateUserPack.csv",sep=",",col.names = FALSE,append = TRUE)
