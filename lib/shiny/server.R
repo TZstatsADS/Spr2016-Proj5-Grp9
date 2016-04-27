6fe368ad2ca96ffd3c249c613a17d2ed05accc22
lllunaliu <ml3810@columbia.edu>
2016-04-27 13:35:17 -0400

    shiny

--- lib/shiny/server.R
+++ lib/shiny/server.R
@@ -2,12 +2,25 @@ library(shiny)
 library(shinydashboard)
 library(plotly)
 library(RColorBrewer)
-
+library('jsonlite')
+library('httpuv')
+library('httr')
+library('RCurl')
+library('psych')
+library('stringr')
+library('fmsb')
+library('recommenderlab')
 ################
 load("year.RData")
 load("1.RData")
 load("2.RData")
 ################
+gtoken <- config(token = github_token)
+pack_class<-read.csv('rclass.csv')
+package_name<-as.character(pack_class[,1])
+class<-table(pack_class[,2])  
+cocur_matrix=read.table('githubco.txt')
+package_name_for_recom<-unique(as.character(pack_class[,1]))
 
 shinyServer(function(input, output,session) {
   
@@ -16,27 +29,27 @@ shinyServer(function(input, output,session) {
   per<-reactive(percent[which(percent$year == input$inputYear),])
   package2<-reactive({input$inputPack2})
   
-# pack rank
+  # pack rank
   output$ranking <- renderValueBox({
     valueBox(packs$rank[which(pack == package())], " Rank",color = "blue")
-    }) 
+  }) 
   
-# Bar Plot of Download Times Pack
+  # Bar Plot of Download Times Pack
   output$barpack <- renderPlotly({
-  times = c(pack[which(pack == package()),][1,2],pack[which(pack == package()),][1,3],pack[which(pack == package()),][1,4],pack[which(pack == package()),][1,5])
-  f <- list(family = "Courier New, monospace", size = 18, color = "#7f7f7f")
-  xlab <- list(title = "year",tick0 = 2011, dtick = 1)
-  ylab <- list(title = "times")
-  plot_ly(x = c("2012","2013","2014","2015"),y = times,opacity = 0.6, type = "bar")%>%
-    layout(xaxis = xlab, yaxis = ylab,title = paste0("Bar Plot of Download Times for ",package()))
+    times = c(pack[which(pack == package()),][1,2],pack[which(pack == package()),][1,3],pack[which(pack == package()),][1,4],pack[which(pack == package()),][1,5])
+    f <- list(family = "Courier New, monospace", size = 18, color = "#7f7f7f")
+    xlab <- list(title = "year",tick0 = 2011, dtick = 1)
+    ylab <- list(title = "times")
+    plot_ly(x = c("2012","2013","2014","2015"),y = times,opacity = 0.6, type = "bar")%>%
+      layout(xaxis = xlab, yaxis = ylab,title = paste0("Bar Plot of Download Times for ",package()))
   })
   
-# pack rank2
+  # pack rank2
   output$ranking2 <- renderValueBox({
     valueBox(packs$rank[which(pack == package2())], " Rank",color = "blue")
   }) 
   
-# Bar Plot of Download Times Pack2
+  # Bar Plot of Download Times Pack2
   output$barpack2 <- renderPlotly({
     times = c(pack[which(pack == package2()),][1,2],pack[which(pack == package2()),][1,3],pack[which(pack == package2()),][1,4],pack[which(pack == package2()),][1,5])
     f <- list(family = "Courier New, monospace", size = 18, color = "#7f7f7f")
@@ -45,22 +58,22 @@ shinyServer(function(input, output,session) {
     plot_ly(x = c("2012","2013","2014","2015"),y = times,opacity = 0.6, type = "bar", color = "orange")%>%
       layout(xaxis = xlab, yaxis = ylab,title = paste0("Bar Plot of Download Times for ",package2()))
   })
-
-#country pieplot
+  
+  #country pieplot
   output$piecoun <- renderPlotly({
     plot_ly(per(), labels = country, values = percent, type = "pie",marker = list(colors = brewer.pal(7, "Paired"))) %>% 
-    layout(title = paste0("Pie Chart of Top 7 Countries in ",years()))
+      layout(title = paste0("Pie Chart of Top 7 Countries in ",years()))
   })
-
-#country scatterplot
+  
+  #country scatterplot
   output$scattercoun <- renderPlotly({
     plot_ly(data = percent, x = year, y = percent, color = country, marker = list(color = brewer.pal(9, "Set1"))) %>% 
       layout(title = paste0("Scatter Plot of Download Times in Each Countries"))
   })
   
   
-    
-#scatterplot rank 30
+  
+  #scatterplot rank 30
   output$plotrank30 <- renderPlotly({
     f2 <- list(family = "Old Standard TT, serif", size = 9, color = "black")
     xlab2 <- list(title = "", tickfont = f2,tickangle = 45)
@@ -68,33 +81,167 @@ shinyServer(function(input, output,session) {
       layout(xaxis = xlab2, title = paste0("Scatter Plot of First 30 Packages "))
   })
   
-#scatterplot for combine packs
+  #scatterplot for combine packs
   output$plotcomb <- renderPlotly({
     f2 <- list(family = "Old Standard TT, serif", size = 5,color = "black")
     xlab2 <- list(title = "Pack Comb", tickfont = f2,tickangle = 45,showticklabels = FALSE)
     plot_ly(data = combine, x = comb, y = count,size = count,color = count,mode = "markers")%>%
       layout(xaxis = xlab2, title = paste0("Scatter Plot of Combination of the first 20 Packages "))
   })
-    
-#group bar chart for os 
+  
+  #group bar chart for os 
   output$baros <- renderPlotly({
-  plot_ly(data = os,x = year, y = value, type = "bar", color = os,marker = list(colors = brewer.pal(5, "Set1"))) %>% 
-    layout(title = paste0("Bar Chart for Number of Users' OS"))
+    plot_ly(data = os,x = year, y = value, type = "bar", color = os,marker = list(colors = brewer.pal(5, "Set1"))) %>% 
+      layout(title = paste0("Bar Chart for Number of Users' OS"))
   })
   
-#scatterplot of clusters
+  #image1
+  output$image1 <- renderImage({
+    list(src = "a.jpg",alt = "Image failed to render",width=650,height=550)
+  }, deleteFile = FALSE)
+  
+  
+  #image2
+  output$image2 <- renderImage({ 
+    list(src = "b.jpg", alt = "Image failed to render",width=650,height=550)
+  }, deleteFile = FALSE)
+  
+  
+  #scatterplot of clusters
   output$cluster <- renderPlotly({
-  plot_ly(data=points_p, x=x,y=y, mode = "markers",text=paste(points_p$rname),color=km$cluster) %>%
-    layout(title = paste0("K-means Cluster of Packages"))
+    plot_ly(data=points_p, x=x,y=y, mode = "markers",text=paste(points_p$rname),color=km$cluster) %>%
+      layout(title = paste0("K-means Cluster of Packages"))
   })
-
-#Heatmap of clusters
+  
+  #Heatmap of clusters
   output$heatmap <- renderPlotly({
     plot_ly(z = corMatrix,x=colnames(corMatrix),y=colnames(corMatrix), type = "heatmap") %>%
       layout(xaxis = list(title=""),yaxis=list(title=""),title = paste0("Heatmap of Packages' Similarity"))
   })
-    
   
-
-  }
+  #############################Github###############################################    
+  info<-reactive({
+    content(GET(paste0("https://api.github.com/users/",input$githubid),gtoken))})
+  info_c<-reactive({
+    content(GET( paste0("https://api.github.com/search/code?page=1&per_page=1000&q=in:file+language:R+user:",input$githubid),gtoken))
+  })
+  
+  
+  r_packageuse<-reactive({
+    r_packageuse<-rep(0,length(package_name))
+    if (info_c()[[1]]==0){
+      
+    }else{
+      for (p in 1:min(100,info_c()[[1]])){
+        r_url=info_c()$items[[p]]$html_url
+        r_url<-gsub('https://github.com/','https://raw.githubusercontent.com/',r_url)
+        r_url<-gsub('/blob','',r_url)
+        rcode=content(GET(r_url))
+        a<-str_count(rcode,package_name)
+        r_packageuse=r_packageuse+a
+      }
+    }
+    r_packageuse
+  })
+  
+  
+  r_packageuse_for_recom<-reactive({
+    r_packageuse_for_recom<-rep(0,length(package_name_for_recom))
+    if (info_c()[[1]]==0){
+      
+    }else{
+      for (p in 1:min(100,info_c()[[1]])){
+        r_url=info_c()$items[[p]]$html_url
+        r_url<-gsub('https://github.com/','https://raw.githubusercontent.com/',r_url)
+        r_url<-gsub('/blob','',r_url)
+        rcode=content(GET(r_url))
+        b<-str_count(rcode,package_name_for_recom)
+        r_packageuse_for_recom = r_packageuse_for_recom+b
+      }
+      
+    }
+    r_packageuse_for_recom
+  })
+  
+  resultlib<-reactive({
+    result<-as.matrix(cocur_matrix)%*%as.matrix(r_packageuse_for_recom())
+    rownames(result)<-package_name_for_recom
+    result1<-result[order(-result[,1]),]
+    
+    
+    index<-which(r_packageuse_for_recom()!=0)
+    libname<-package_name[index]
+    
+    index<-which(names(result1) %in% libname)
+    result1<-result1[-index]
+    names(result1[1:5])  
+  })
+  
+  
+  
+  
+  
+  
+  
+  radar_date<-reactive({
+    class_use<-rep(0,length(class))
+    for(i in 1:length(package_name)){
+      for (j in 1:length(class)){
+        if(pack_class[i,2]==names(class)[j]){
+          class_use[j]=class_use[j]+r_packageuse()[i]
+        }
+      }
+    }
+    
+    radar_date<-data.frame(rbind(rep(max(class_use),33), rep(0,33), class_use))
+    colnames(radar_date)<-names(class)
+    radar_date
+  })
+  
+  
+  output$repnumBox <- renderValueBox({
+    valueBox(
+      info()$public_repos, "Repositories", icon =icon("code-fork"),
+      color = "orange")
+  })
+  
+  output$rscriptBox <- renderValueBox({
+    valueBox(
+      info_c()[[1]], "R Scripts", icon = icon("code"),
+      color = "blue")
+  })
+  
+  output$rlibraryBox <- renderValueBox({
+    valueBox(
+      sum(r_packageuse()!=0), "R libraries", icon = icon("hand-peace-o"),
+      color = "green")
+  })
+  
+  output$githubinfo<-renderUI({
+    str0<-"User Infomation:"
+    str1<-paste("User ID: ", input$githubid)
+    str2<-paste("Following: ",info()$following)
+    str3<-paste("Follower: ",info()$followers)
+    str4<-paste("Join Date: ",info()$created_at)
+    str5<-paste("Last time update: ",info()$updated_at)
+    HTML(paste(h4(str0),h5(str1),h5(str2),
+               h5(str3),h5(str4),h5(str5), 
+               sep = '<br>'))
+  })
+  
+  output$librecom<-renderUI({
+    str0<-"Our Recommendations for you:"
+    str1<-resultlib()[1]
+    str2<-resultlib()[2]
+    str3<-resultlib()[3]
+    str4<-resultlib()[4]
+    str5<-resultlib()[5]
+    HTML(paste(h5(str0),h5(str1),h5(str2),
+               h5(str3),h5(str4),h5(str5), 
+               sep = '<br>'))})
+  output$radar<-renderPlot({
+    radarchart(radar_date(),cglcol='cyan',cglwd=1,pfcol='royalblue3',seg=3,cglty=1,pty=32,plwd=5,pdensity=100,vlcex=0.8,calcex=100)
+  })
+  
+}
 )
--- lib/shiny/ui.R
+++ lib/shiny/ui.R
@@ -10,6 +10,14 @@ library(lubridate)
 library(dygraphs)
 library(xts)
 library(leaflet)
+library('jsonlite')
+library('httpuv')
+library('httr')
+library('RCurl')
+library('psych')
+library('stringr')
+library('fmsb')
+library('recommenderlab')
 
 ################
 load("year.RData")
@@ -17,6 +25,8 @@ load("1.RData")
 load("2.RData")
 ################
 shinyUI( fluidPage(
+  includeCSS(path = "AdminLTE.css"),
+  includeCSS("shinydashboard.css"),
   navbarPage(
     "R Package Mining",
     tabPanel("Analysis",icon=icon("line-chart"),
@@ -25,75 +35,123 @@ shinyUI( fluidPage(
                                         conditionalPanel(condition = "input.conditionedPanels == 0",
                                                          p(strong("*"),"The first three popular packages from 2012 to 2015 were:",br(),code("Rcpp"), code("ggplot2"),code("Plyr")),
                                                          p(strong("*"),"The first three popular combination of packages from 2012 to 2015 were:",br(),code("digest & stringr"), code("colorspace & ggplot"),code("reshape2 & ggplot2"))
-                                                         ),
+                                        ),
                                         conditionalPanel(condition = "input.conditionedPanels == 1",
+                                                         p(strong("*"),"If we use install.packages",code("ggplot2"),"to download package, it automatically install its dependencies. Also, we need to install its import packages when we library",code("ggplot2"),"."),
+                                                         p(strong("*"),"In the package we find three more packages of the Rcpp family:",br(),code("Rcpp"), br(),code("RcppArmadillo"), br(),code("RcppEigen"),br(), "but only a limited number of other packages as C/C++ level dependencies are still somewhat rare in the R universe.")
+                                        ),
+                                        conditionalPanel(condition = "input.conditionedPanels == 2",
                                                          selectInput("inputPack", "Input Package:", levels(packs$pack),multiple=T,selected = "Amelia"),
                                                          selectInput("inputPack2", "Input Another Package:", levels(packs$pack),multiple=T,selected = "Amelia")
-                                                         ),
-                                        conditionalPanel(condition = "input.conditionedPanels == 2",
+                                        ),
+                                        conditionalPanel(condition = "input.conditionedPanels == 3",
                                                          selectInput("inputYear", "Select Year:",
                                                                      choices = list(2012,2013,2014,2015)),
                                                          p(strong("*"),code("U.S."),"had the most download times from 2012 to 2015."),
-                                                         p(strong("*"),code("China"),"had signficant increased download times from 2012 to 2015.")
-                                                         ),
+                                                         p(strong("*"),code("China"),"had a strong grwoth of download times from 2012 to 2015.")
+                                        ),
                                         conditionalPanel(condition = "input.conditionedPanels == 4",
                                                          p(strong("*"),code("mingw32"),code("linux-gnu"),"were the most popular OS for users to download packages from 2012 to 2015.")
-                                                         )
-                                                         
+                                        )
+                                        
                            ),
                            
                            mainPanel(width = 10,tabsetPanel(id = "conditionedPanels", type="pill",
                                                             tabPanel("Overview",br(),
                                                                      fluidRow(
-                                                                       column(6,plotlyOutput("plotrank30", width="500px",height="500px")),
-                                                                       column(6,plotlyOutput("plotcomb", width="500px",height="500px"))
+                                                                       column(6,plotlyOutput("plotrank30")),
+                                                                       column(6,plotlyOutput("plotcomb"))
                                                                      ),value = 0),
-                                                            
+                                                            tabPanel("Combination",br(),
+                                                                     fluidRow(
+                                                                       h5(strong("Relations for Top 20 Packages")),
+                                                                       column(6,imageOutput("image1", width = "100%", height = 500)),
+                                                                       column(6,imageOutput("image2", width = "100%", height = 500))
+                                                                     ),value = 1
+                                                            ),
                                                             tabPanel("Package",br(),
                                                                      fluidRow(
                                                                        column(1,valueBoxOutput("ranking")),
-                                                                       column(9,plotlyOutput("barpack"))),
-                                                                     fluidRow(
+                                                                       column(5,plotlyOutput("barpack")),
                                                                        column(1,valueBoxOutput("ranking2")),
-                                                                       column(9,plotlyOutput("barpack2"))
-                                                                     ),value = 1
+                                                                       column(5,plotlyOutput("barpack2")))
+                                                                     ,value = 2
                                                             ),
                                                             
                                                             tabPanel("Country",br(),
                                                                      fluidRow(
                                                                        column(6, plotlyOutput("scattercoun",width="500px",height="500px")),
                                                                        column(6, plotlyOutput("piecoun",width="500px",height="500px"))
-                                                                     ),value = 2),
+                                                                     ),value = 3),
                                                             tabPanel("OS",br(),
-                                                                       plotlyOutput("baros", width="900px",height="500px"
-                                                                                    ),value = 4)
-                                     
+                                                                     plotlyOutput("baros", width="900px",height="500px"
+                                                                     ),value = 4)
+                                                            
+                                                            
                                                             
                            ))
              )),
-    tabPanel("Similarity",icon=icon("spinner"),
+    tabPanel("Cluster",icon=icon("spinner"),
              sidebarLayout(position="left",
-             sidebarPanel(width = 2,
-                          conditionalPanel(condition = "input.conditionedPanels == 0",
-                                           p(strong("Cluster Methods:")),
-                                           p(strong("*"),"Calculate the word frequency of each packages' description."),
-                                           p(strong("*"),"Create the", code("latent semantic space")),
-                                           p(strong("*"),"Remove stop words and select 1000 most popular packages"),
-                                           p(strong("*"),"Compute the distance between every two words using",code("TFIDF")),
-                                           p(strong("*"),"Reduce Dimension using",code("CMD")),
-                                           p(strong("*"),"Cluster the packages using",code("K-means"))
-                                            ),
-                          conditionalPanel(condition = "input.conditionedPanels == 1"
-                          )
-                          ),
-             mainPanel(width = 10,tabsetPanel(id = "conditionedPanels", type="pill",
-                                              tabPanel("Scatterplot",br(),
-                                                       plotlyOutput("cluster", width="900px",height="500px"
-                                                       ),value = 0),
-                                              tabPanel("Heatmap",br(),
-                                                       plotlyOutput("heatmap", width="900px",height="500px"
-                                                       ),value = 1)
-             )
-             )))
-             
+                           sidebarPanel(width = 2,
+                                        p(strong("Cluster Methods:")),
+                                        p(strong("*"),"Calculate the word frequency of each packages' description."),
+                                        p(strong("*"),"Create the", code("latent Semantic Space")),
+                                        p(strong("*"),"Remove stop words and select 1000 most popular packages"),
+                                        p(strong("*"),"Compute the distance between every two words using",code("TFIDF")),
+                                        p(strong("*"),"Reduce Dimension using",code("CMD")),
+                                        p(strong("*"),"Cluster the packages using",code("K-Means"))
+                                        
+                           ),
+                           mainPanel(width = 10,tabsetPanel(type="pill",
+                                                            tabPanel("Scatterplot",br(),
+                                                                     plotlyOutput("cluster", width="900px",height="500px"
+                                                                     )),
+                                                            tabPanel("Heatmap",br(),
+                                                                     plotlyOutput("heatmap", width="900px",height="500px"
+                                                                     ))
+                           )
+                           ))),
+    #########################Github#############################
+    tabPanel("Conplementary libraries",icon=icon("space-shuttle"),
+             fluidRow(
+               
+               
+               
+               h5("Explore more:",
+                  a(href = "http://weihan.net16.net/", icon("hand-scissors-o"))),
+               img(src="dep.jpg",width=1200),
+               h5("We scraped the data from the log info from Rstudio webstie which ranges from 2012 to 2016. To explore more about the dependencies 
+                  between the libraries,please click top left hand")
+               
+               
+               )),
+    
+    
+    
+    #################### end of github####################
+    
+    #########################Github#############################
+    tabPanel("Github Analysis",icon=icon("github-alt"),
+             fluidRow(
+               column(width = 4,
+                      box(width = NULL, 
+                          textInput("githubid", label = h3("Plase Enter your Github id"), 
+                                    value = "tz33cu")),
+                      box(width=NULL,
+                          uiOutput("githubinfo")),
+                      
+                      box(width=NULL,
+                          uiOutput("librecom"))
+               ),
+               column(width = 8,
+                      box(width = NULL,
+                          valueBoxOutput("repnumBox"),
+                          valueBoxOutput("rscriptBox"),
+                          valueBoxOutput("rlibraryBox"),
+                          plotOutput("radar", height = 800))
+               )
+             ))
+    
+    
   )))
